4c702a70649aa7bb135b65923465b7ef3180db5b
Finazzer Matteo (ACC0071385) <matteo.finazzer@debian.UNITN.IT>
2018-04-04 17:33:16 +0200

    init te mouse project

--- mouseTest/battleship.v
+++ mouseTest/battleship.v
@@ -1,5 +1,6 @@
 //`timescale 1ns / 1ps
 `define		defaultPeriod	30'b000001011111010111100001000000	//	25 10^6
+`define mss 30'd5000
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -21,18 +22,21 @@
 //////////////////////////////////////////////////////////////////////////////////
 module battleship(CLK_50M,
 
-				LED);
+				LED,
+				PS2_DATA1
+				);
 
 input		CLK_50M; //here decalrations, variable with 1 bit.
-output	[7:0]	LED;// so have low impedence. 8 digits. is a bus. 
+output	[7:0]	LED;// so have low impedence. 8 digits. is a bus.
+output	PS2_DATA1;
 
 // here wires, modules, ecc.
 
-// Module_FrequencyDivider		clock_1_Hz_generator(	.clk_in(CLK_50M),           //is another 'chip'
-//								.period(`defaultPeriod),
-//
-//							.clk_out(w_clock_1_Hz));
+ Module_FrequencyDivider		clock_10_kHz_generator(	.clk_in(CLK_50M),           //is another 'chip'
+								.period(`mss),
 
+						.clk_out(PS2_DATA1));
 
+assign LED=8'b10101010;
 
 endmodule
