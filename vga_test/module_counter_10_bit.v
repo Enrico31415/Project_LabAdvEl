00954db28a0ce950e8600d1bc18cae177940ad3c
Pierobon Enrico (ACC0070440) <enrico.pierobon@debian.UNITN.IT>
2018-04-05 17:50:19 +0200

    VGA funzionane, con implementazione simulazione Mouse

--- vga_test/module_counter_10_bit.v
+++ vga_test/module_counter_10_bit.v
@@ -1,12 +1,3 @@
-`timescale 40ns/40ps
-
-`define red 	12'b111100000000
-`define green 	12'b000011110000
-`define blue 	12'b000000001111
-`define black 	12'b000000000000
-
-
-
 //parametri del counter in lunghezza (h)
 `define		hmonitor_lenght 10'd800 //	800 lunghezza schermo
 `define		hcursor_length	10'd640	//	lunghezza utilizzabile (dal cursore)
@@ -27,7 +18,9 @@ module	Module_Counter_10_bit	(	clk_in,
 
 					out_hsync,
 					out_vsync,
-					test_color
+					write_enable,
+					out_rows,
+					out_lines
 					);
 
 
@@ -36,10 +29,13 @@ input		clk_in;
 reg [9:0] rows = 10'd0; 
 reg [9:0] lines = 10'd0; 
 
-output reg out_hsync;
-output reg out_vsync;
+output reg [9:0] out_rows = 10'b0000000000; 
+output reg [9:0] out_lines = 10'b0000000000; 
 
-output reg [11:0] test_color = `black;
+output reg out_hsync = 1;
+output reg out_vsync = 1;
+
+output reg write_enable = 0;
 
 
 reg [5:0] status = 4'b0;
@@ -48,23 +44,35 @@ reg [5:0] vstatus = 4'b0;
 
 
 always @(posedge clk_in) begin
+
+	if(out_rows > `hcursor_length -10'b0000000001 && out_rows != 10'b1111111111)
+	begin
+		out_rows = 0;
+	end
+	
+	if(out_lines > `vcursor_length -10'b0000000001 && out_lines != 10'b1111111111)
+	begin
+		out_lines = 0;
+	end
+
+
 	// inizio facendo un contatore fino alla lugnhezza del monitor
-	if(rows >  `hmonitor_lenght - 10'd0000000001) //serve a far tornar i conti //capolinea righe
+	if(rows >  `hmonitor_lenght - 10'b0000000001) //serve a far tornar i conti //capolinea righe
 	begin
 		rows = 0;
 		lines = lines +1;
 		out_hsync = 1;
 		out_vsync = 1;
-		test_color = `black;
+		write_enable = 0;
 		status = 5'b00000;
 		vstatus = 5'b00000;
-		if(lines >   `vmonitor_lenght - 10'd0000000001) //capolinea linee (gioco di parole)
+		if(lines >   `vmonitor_lenght - 10'b0000000001) //capolinea linee (gioco di parole)
 		begin
 			lines = 0;
 			rows = 0;
 			out_hsync = 1;
 			out_vsync = 1;
-			test_color = `black;
+			write_enable = 0;
 		end		
 	end
 	if(rows >= 0 && rows < `hfp_length) //48 cicli o 1.92us FP
@@ -73,21 +81,21 @@ always @(posedge clk_in) begin
 		out_hsync = 1;
 		out_vsync = 1;
 		rows = rows +1;
-		test_color = `black;
+		write_enable = 0;
 	end	
 	else if (rows >= `hfp_length && rows < (`hfp_length+`hsync_length)) //16 cicli o 640ns SYNCPULSE
 	begin
 		status = 5'b00010;
 		out_hsync = 0;
-		rows = rows +1;
-		test_color = `black;
+		rows = rows + 1;
+		write_enable = 0;
 	end
 	else if (rows >= (`hfp_length+`hsync_length) && rows < (`hfp_length + `hsync_length + `hbp_length)	)//16 cicli o 640ns BP
 	begin
 		status = 5'b00011;
 		out_hsync = 1;
 		rows = rows +1;
-		test_color = `black;
+		write_enable = 0;
 	end
 	else if (rows >= (`hfp_length + `hsync_length + `hbp_length) && rows <  `hmonitor_lenght ) // sono nell'area visibile h
 	begin 
@@ -98,7 +106,9 @@ always @(posedge clk_in) begin
 		begin
 			vstatus = 5'b00100;
 			out_vsync = 1;
-			test_color = `red;
+			out_lines = lines - (`vfp_length + `vsync_length + `vbp_length) ; //aggiorna la posizione
+			out_rows = rows - (`hfp_length + `hsync_length + `hbp_length); //aggiorna la posizione
+			write_enable = 1;
 		end
 	end
 	// Condizioni sulle linee
@@ -107,19 +117,19 @@ always @(posedge clk_in) begin
 		vstatus =  5'b00001;
 		out_vsync = 1;
 		//rows = rows +1;
-		test_color = `black;
+		write_enable = 0;
 	end
 	else if (lines >= `vfp_length && lines < (`vfp_length + `vsync_length)) //PULSE
 	begin
 		vstatus = 5'b00010;
 		out_vsync = 0;
-		test_color = `black;
+		write_enable = 0;
 	end
 	else if (lines >= (`vfp_length + `vsync_length) && lines < (`vfp_length + `vsync_length + `vbp_length)) //BP
 	begin
 		vstatus = 5'b00011;
 		out_vsync = 1;
-		test_color = `black;
+		write_enable = 0;
 	end
 	else if (lines >= (`vfp_length + `vsync_length + `vbp_length) && lines < `vmonitor_lenght) //Se ho appena proiettao a schermo
 	begin
@@ -130,7 +140,7 @@ always @(posedge clk_in) begin
 	begin
 		out_hsync = 1;
 		out_vsync = 1;
-		test_color = `black;
+		write_enable = 0;
 		status = 5'b10000;
 		vstatus = 5'b10000;
 	end*/
