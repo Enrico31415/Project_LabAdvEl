6ad7d60ea4a82564714b2e6f5f6c434d761c274c
Pierobon Enrico (ACC0070440) <enrico.pierobon@debian.UNITN.IT>
2018-04-11 13:42:17 +0200

    Bozza del core. Ancora tante domande.

---
+++ Core/RN/RandomGenerator/invertent_counter.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:23:19 11/08/2017 
+// Design Name: 
+// Module Name:    invertent_counter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module invertent_counter(
+    );
+
+
+endmodule
