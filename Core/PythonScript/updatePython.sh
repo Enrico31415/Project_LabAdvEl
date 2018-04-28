e85715e070262d3bf10239670a90b83894afb6dc
Enrico31415 <enrico.pierobon@studenti.unitn.it>
2018-04-28 18:04:49 +0200

    generato cerchio

--- Core/PythonScript/updatePython.sh
+++ Core/PythonScript/updatePython.sh
@@ -1,2 +1,9 @@
 python3 positionToQuadrant.py > ../pos_to_quadrant.v
 python3 memoryGenerator.py > ../cellMemory.v
+python3 printCircle.py > circle.txt
+python3 printCross.py > cross.txt
+cross=$(<cross.txt)
+circle=$(<circle.txt)
+cross=${cross:6}
+circle=${circle:6}
+
