6d47836e5245d335110f8006260378171fd9db10
mate377 <matteo.finazzer@gmail.com>
2018-05-14 11:05:01 +0200

    Position bugs are solved (where overflow).

--- vga_test/mouse_data_management.v
+++ vga_test/mouse_data_management.v
@@ -35,20 +35,26 @@ input [7:0] deltaX;
 input [7:0] deltaY;
 input tx;
 		
-output reg [9:0] posX=10'd0;
-output reg [9:0] posY=10'd0;
+output reg [10:0] posX=11'd0;
+output reg [10:0] posY=11'd0;
 
 reg tx_old;
 // do the 2 bit complement
 always @ (posedge qzt_clk) begin
 	if (!tx_old & tx) begin
-		posX<=posX+{status[4],status[4],deltaX};
-		posY<=posY+ ~{status[5],status[5],deltaY} + 1;
+		posX<=posX+{{3{status[4]}},deltaX};
+		posY<=posY+ ~{{3{status[5]}},deltaY} + 1;
 	end else begin
+		if (posX[10] || (!posX[10] && posX<=11'd9))	posX<=11'd10;
+		if (!posX[10] && posX[9:0]>=11'd640)			posX<=11'd640;
+		if (posY[10] || (!posY[10] && posY<=11'd9))	posY<=11'd10;
+		if (!posY[10] && posY[9:0]>=11'd480)			posY<=11'd480;
+		/*
 		if (posX[9])				posX<=posX + 10'd640;
 		if (posX>=10'd639)	posX<=posX + (~ 10'd640) + 1;
 		if (posY[9])				posY<=posY + 10'd480;
 		if (posY>=10'd479)	posY<=posY + (~ 10'd480) + 1;
+		*/
 	end
 	tx_old<=tx;
 end
